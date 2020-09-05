d6b03704f0436a9c1a2368b2b967de82b8f86366
Patbodin Intajak <patbodin30443@hotmail.com>
2020-09-05 18:49:02 +0700

    Add docker-compose installation

--- Installation_Guide/CentOS8/install_docker.sh
+++ Installation_Guide/CentOS8/install_docker.sh
@@ -6,7 +6,7 @@
 ######################
 
 #Install Docker
-echo "-- [Docker Installation] --"
+echo "-- [Docker & Docker-Compose Installation] --"
 echo "-- [Docker Installation: Start Process] --"
 
 #Setup repository
@@ -36,5 +36,18 @@ usermod -aG docker poseidon
 chmod 666 /var/run/docker.sock
 
 echo "-- [Docker Installation: End Process] --"
+echo "-- [Docker-Compose Installation: Start Process] --"
+
+#Install docker-compose
+dnf install curl -y
+
+curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+#Show docker-compose version
+docker-compose --version
+
+echo "-- [Docker-Compose Installation: End Process] --"
 
 ##########################################
