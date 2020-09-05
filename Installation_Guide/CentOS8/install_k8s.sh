bbaf70fce03dd9433f7803230534adae387d3f93
Patbodin Intajak <patbodin30443@hotmail.com>
2020-09-05 19:43:26 +0700

    Add installation for kubernetes

---
+++ Installation_Guide/CentOS8/install_k8s.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Login to super user first
+######################
+# su -
+######################
+
+#Install Docker
+echo "-- [Kubernetes Installation] --"
+echo "-- [Kubernetes Installation: Start Process] --"
+
+#Set bridged packets to traverse iptables rules
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+	
+sysctl --system
+
+swapoff -a
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+dnf update -y
+
+#Install all the necessary components for Kubernetes
+dnf install -y kubelet kubeadm kubectl kubernetes-cni --disableexcludes=kubernetes
+
+#Start k8s
+systemctl enable kubelet
+systemctl start kubelet
+
+echo "-- [Kubernetes Installation: End Process] --"
+
+##########################################
